a181cbbf6365c936ef0fe21f813766c80613ea06
杨伟波 <yangweibo@sanhao.com>
2017-02-06 16:16:18 +0800

    添加文件

---
+++ mcrypt.php
@@ -0,0 +1,62 @@
+<?php
+
+class mcryptExt {
+
+    /**加密
+     * @param $input
+     * @param $key
+     * @return string
+     */
+    public static function des_ecb_encrypt($input, $key) {
+        $size = mcrypt_get_block_size('des', 'ecb');
+        $input = self::pkcs5_pad($input, $size);
+        $td = mcrypt_module_open('des', '', 'ecb', '');
+        $iv = @mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        @mcrypt_generic_init($td, $key, $iv);
+        $data = mcrypt_generic($td, $input);
+        mcrypt_generic_deinit($td);
+        mcrypt_module_close($td);
+        $data = base64_encode($data);
+        return $data;
+    }
+
+    /**
+     * 解密
+     * @param $encrypted
+     * @param $key
+     * @return bool|string
+     */
+    public static function des_ecb_decrypt($encrypted, $key) {
+        $encrypted = base64_decode($encrypted);
+        $td = mcrypt_module_open('des','','ecb','');
+        //使用MCRYPT_DES算法,cbc模式
+        $iv = @mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        $ks = mcrypt_enc_get_key_size($td);
+        @mcrypt_generic_init($td, $key, $iv);
+        //初始处理
+        $decrypted = mdecrypt_generic($td, $encrypted);
+        //解密
+        mcrypt_generic_deinit($td);
+        //结束
+        mcrypt_module_close($td);
+        $y=self::pkcs5_unpad($decrypted);
+        return $y;
+    }
+
+    private static function pkcs5_pad ($text, $blocksize) {
+        $pad = $blocksize - (strlen($text) % $blocksize);
+        return $text . str_repeat(chr($pad), $pad);
+    }
+
+    private static function pkcs5_unpad($text) {
+        $pad = ord($text{strlen($text)-1});
+        if ($pad > strlen($text))
+            return false;
+        if (strspn($text, chr($pad), strlen($text) - $pad) != $pad)
+            return false;
+        return substr($text, 0, -1 * $pad);
+    }
+}
+
+
+

be9a14d00e37a4a1e4564f9285dd32fc1c0836ba
vinbel <vbgo_vip@163.com>
2017-04-10 16:43:10 +0800

    modify a notice

--- protected/backend/index.php
+++ protected/backend/index.php
@@ -4,14 +4,22 @@ define('DEBUG', true);
 require_once WEB_ROOT.'common.php';
 define('MODULE', WEB_ROOT . 'module/');
 $_REQUEST['server_mod'] = 'cli';
+
 if(!empty($argv)) {
     $r = '';
     $r_arr = $argv;
     unset($r_arr[0]);
 
-    $r = implode('/', $r_arr);
+    if(empty($r_arr)) {
+        echo 'please into route!';
+        exit();
+    }
 
+    $r = implode('/', $r_arr);
     $_REQUEST['r'] = $r;
+} else {
+    echo 'please into route!';
+    exit();
 }
 
 $_POST = $_GET = $_REQUEST;
